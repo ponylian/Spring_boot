02e429b1ce04a11c7049fd65c640bc4bcbb1c853
ponylian <769624572@qq.com>
2018-10-27 21:20:38 +0800

    s

--- src/main/java/com/lian/springboot/HelloController.java
+++ src/main/java/com/lian/springboot/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String index(){
-        return "helel----";
+        return "helel--dddd--";
 
     }
 
